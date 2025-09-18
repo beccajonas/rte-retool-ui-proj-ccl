a927df57012b728dee83f6fc9c4db359cfc07d0b
Becca Jonas <becca@retool.com>
2025-09-18 15:25:15 -0400

    make link popover smaller

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ export function richTextEditor() {
 
   const [macro, _setMacros] = Retool.useStateString({
     name: "macro",
-    initialValue: "My macro"
+    initialValue: "My macro!"
   })
 
   return (
